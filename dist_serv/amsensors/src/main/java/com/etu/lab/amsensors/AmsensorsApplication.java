13bf7c7b85f5341ed542973ceeb7362f9f816339
Stepan Repin <stnrepin@gmail.com>
2022-11-20 16:55:31 +0300

    dist_serv: Добавил вторую лабу

--- dist_serv/amsensors/src/main/java/com/etu/lab/amsensors/AmsensorsApplication.java
+++ dist_serv/amsensors/src/main/java/com/etu/lab/amsensors/AmsensorsApplication.java
@@ -2,12 +2,34 @@ package com.etu.lab.amsensors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class AmsensorsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmsensorsApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmsensorsApplication.class, args);
-	}
+    @Bean
+    public LocaleResolver localeResolver() {
+       SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+       Locale ru = new Locale("en", "US");
+       localeResolver.setDefaultLocale(ru);
+       return localeResolver;
+    }
 
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+       ResourceBundleMessageSource messageSource =
+           new ResourceBundleMessageSource();
+       messageSource.setUseCodeAsDefaultMessage(true);
+       messageSource.setBasenames("messages");
+       messageSource.setDefaultEncoding("UTF-8");
+       return messageSource;
+    }
 }
